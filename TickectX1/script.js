548d3d95463b9f008942fb49250a3e3ca4135d31
PrabhashiDileshika <stu03001421@dpf.edu.lk>
2024-05-02 14:42:23 +0530

    first commit

---
+++ TickectX1/script.js
@@ -0,0 +1,28 @@
+
+        // Get a reference to the button
+        var viewMarketplaceBtn = document.getElementById("viewMarketplaceBtn");
+
+        // Define the function to navigate to View Marketplace.html
+        function goToMarketplace() {
+            window.location.href = "/View Marketplace.html";
+        }
+
+        // Attach an event listener to the button, listening for the "click" event
+        viewMarketplaceBtn.addEventListener("click", goToMarketplace);
+
+        function button1() {
+            window.location.href = "index.html";
+        }
+
+        function button2() {
+            window.location.href = "welcomeback.html";
+        }
+
+        function button3() {
+            window.location.href = "/View Marketplace.html";
+        }
+
+
+
+
+        
